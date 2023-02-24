28ff6d4a6280d164dc6743845354d8bc13012900
Alma Ordoñez <alma.ordonez2002@gmail.com>
2023-02-24 17:57:50 -0600

    feat: Front select options, not working

--- PrimeraEntrega/rss-reader/src/pages/Sections/_feed.tsx
+++ PrimeraEntrega/rss-reader/src/pages/Sections/_feed.tsx
@@ -2,6 +2,10 @@ import { useEffect, useRef, useState } from "react"
 import NewsCard from "../../abstractComponents/NewsCard"
 import { WebNews } from 'public/interface/WebNews';
 import  { News }  from 'public/interface/NewsInfo';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
 
 export default function _feed(){
     var debounce = require('lodash.debounce');
@@ -74,13 +78,39 @@ export default function _feed(){
         debounced();
     };
 
+    const [age, setAge] = useState('');
+
+    const handleChange = (event: SelectChangeEvent) => {
+        setAge(event.target.value);
+    };
+
     return(
         <div className="vstack gap-3 justify-content-center">
+            <center>
             <h1>Feed</h1>
             <input id="search" type={'text'} className='form-control' style={{width:'300px'}}  onChange={(event:any)=>{debounceSearch(event)}} autoComplete={"off"}/>
+            <br></br>
+            <InputLabel id="demo-simple-select-autowidth-label">Buscar por tipo</InputLabel>
+            <Select
+            labelId="demo-simple-select-autowidth-label"
+            id="demo-simple-select-autowidth"
+            value={age}
+            onChange={handleChange}
+            autoWidth
+            label="Age"
+            >
+            <MenuItem value="">
+                <em>None</em>
+            </MenuItem>
+            <MenuItem value="title">Título</MenuItem>
+            <MenuItem value="category">Categoría</MenuItem>
+            <MenuItem value="description">Descripción</MenuItem>
+            </Select>
+            <br></br><br></br><br></br>
             {
                filteredNews.length > 0 && filteredNews.map((item,index)=>{return <NewsCard key={index} news={item} />})
             }
+            </center>
         </div>
     )
 }
