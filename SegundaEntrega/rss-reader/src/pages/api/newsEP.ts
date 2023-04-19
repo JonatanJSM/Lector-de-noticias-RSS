ecef4f88de96aa683206f150c6a070804bb330de
Fernan Cetina <fernancetina@Fernan.local>
2023-04-19 16:26:26 -0600

    test: change title of first news

--- SegundaEntrega/rss-reader/src/pages/api/newsEP.ts
+++ SegundaEntrega/rss-reader/src/pages/api/newsEP.ts
@@ -16,7 +16,8 @@ export default async function handler(req:NextApiRequest, res:NextApiResponse) {
         
         if(req.method === 'GET'){
             const newsDb:WebNews[] = await db.collection("news").find({}).toArray();
-            newss = newsDb;            
+            newss = newsDb;  
+            newss[0].title = newss[0].title + "!!";         
         }
 
         if(req.method === 'POST'){
